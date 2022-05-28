3082e5498f373ee1bb5b07303fd976d1fda7f02e
harishkumarforever <harishking809@gmail.com>
2022-05-28 16:31:20 +0530

    Create remove duplicate values and just key unique values.js

---
+++ remove duplicate values and just key unique values.js
@@ -0,0 +1,7 @@
+let uniqueItems = [...new Set(items)]
+
+
+// output : [4, 5, 6, 3, 2, 23, 1]
+
+
+link :: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
