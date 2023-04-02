40a0f85d793b5fd133d3c118e7668e9c152b6879
harishkumarforever <harishking809@gmail.com>
2023-04-02 10:08:10 +0530

    Create logout acces token.js

---
+++ logout acces token.js
@@ -0,0 +1,50 @@
+//  these is auth middle mare
+
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if (!user) {
+            throw new Error()
+        }
+
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
+}
+
+module.exports = auth
+
+
+
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
