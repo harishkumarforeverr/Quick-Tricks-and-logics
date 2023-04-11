72d1a0d40f15f63e483af7cab45a174c910d4c02
harishkumarforever <harishking809@gmail.com>
2023-04-11 17:03:46 +0530

    Create dwd a local file onClick buttton.js

---
+++ Excel file/dwd a local file onClick buttton.js
@@ -0,0 +1,7 @@
+
+import sampleExcel from "./sampleExcel.xlsx"; 
+import { Breadcrumb, Divider, Spin, Icon, Button, Upload, message } from "antd";
+
+<a href={sampleExcel} download="sampleExcel" target="_blank">
+       <Button type="primary">sample file</Button>
+ </a>
