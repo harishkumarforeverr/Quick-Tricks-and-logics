f11e65eb061e5c7fe93ea11d5b74ed737bb30f80
harishkumarforever <harishking809@gmail.com>
2023-05-03 13:58:55 +0530

    Create Generate random number between two numbers in JavaScript.js

---
+++ Generate random number between two numbers in JavaScript.js
@@ -0,0 +1,9 @@
+1. https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const rndInt = randomIntFromInterval(1, 6)
+console.log(rndInt)
