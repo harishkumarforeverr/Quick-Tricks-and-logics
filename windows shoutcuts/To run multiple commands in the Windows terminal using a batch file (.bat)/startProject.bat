abad0fa9157a7903a58fb9c42be3b6c830c06d17
harishkumarforever <harishking809@gmail.com>
2023-07-04 10:11:53 +0530

    Create startProject.bat

---
+++ windows shoutcuts/To run multiple commands in the Windows terminal using a batch file (.bat)/startProject.bat
@@ -0,0 +1,13 @@
+@echo off
+
+cd /d D:\dev2prod\Provider_Project\ub_api_provider
+start code .
+
+cd /d D:\dev2prod\userApp_Project\ub_api_user
+start code .
+
+cd /d D:\dev2prod\Provider_Project\ub_provider
+start code .
+
+cd /d D:\dev2prod\userApp_Project\ub_web_user
+start code .
